a15ae01a4127df310451a8a4953088051d82534c
Dennis Haegler <dennis.haegler@gmail.com>
2017-01-05 21:54:29 +0100

    Init commit

---
+++ main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"chat/trace"
+	"flag"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"sync"
+
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/gomniauth/providers/facebook"
+	"github.com/stretchr/gomniauth/providers/github"
+	"github.com/stretchr/gomniauth/providers/google"
+)
+
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates",
+			t.filename)))
+	})
+	t.templ.Execute(w, r)
+}
+
+func main() {
+	var addr = flag.String("addr", ":8080", "The addr of the application.")
+	flag.Parse()
+
+	gomniauth.SetSecurityKey("HIMYNAMEISCOOJOE")
+	gomniauth.WithProviders(
+		google.New("871080671786-190pogj8i6b95fshfai9bt0vgqgai371.apps.googleusercontent.com",
+			"jJBzeKcrNfhrgDpY8QX2aPBd",
+			"http://localhost:8080/auth/callback/google"),
+		github.New("38a483ebc0beb5af8eaf", "8f34eded82eb0db9ac7ef3bccc20ae113711ff4b",
+			"http://localhost:8080/auth/callback/github"),
+		facebook.New("key", "secret",
+			"http://localhost:8080/auth/callback/facebook"),
+	)
+	r := newRoom()
+	r.tracer = trace.New(os.Stdout)
+
+	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
+	http.Handle("/login", &templateHandler{filename: "login.html"})
+	http.HandleFunc("/auth/", loginHandler)
+	http.Handle("/room", r)
+
+	go r.run()
+
+	fmt.Println("Server startet on port", *addr)
+
+	if err := http.ListenAndServe(*addr, nil); err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
---
+++ trace/tracer_test.go
@@ -0,0 +1,25 @@
+package trace
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestNew(t *testing.T) {
+	var buf bytes.Buffer
+	tracer := New(&buf)
+	if tracer == nil {
+		t.Error("Return from New should not be nil")
+	} else {
+		myText := "Hello trace package"
+		tracer.Trace(myText)
+		if buf.String() != myText+"\n" {
+			t.Errorf("Trace should not write '%s'.", buf.String())
+		}
+	}
+}
+
+func TestOff(t *testing.T) {
+	var silentTracer Tracer = Off()
+	silentTracer.Trace("something")
+}
